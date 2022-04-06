271ae8190f87ca05281617502c1da0230a687c3a
William Pei <draekien@users.noreply.github.com>
2022-04-06 17:02:08 +1000

    feat(spinner): exported the spinner component

--- packages/waystone-components/src/index.tsx
+++ packages/waystone-components/src/index.tsx
@@ -2,5 +2,6 @@ export * from './anchor/anchor';
 export * from './button/button';
 export * from './code/code';
 export * from './icon/icon';
+export * from './spinner/spinner';
 export * from './text/text';
 export * from './waystone-ui/waystoneUi';
