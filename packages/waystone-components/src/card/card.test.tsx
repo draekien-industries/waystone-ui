c2d927c3add76f75778f39c0a0f64f1a08259a33
William Pei <draekien@users.noreply.github.com>
2022-05-06 15:35:19 +1000

    test(card): added another snapshot test

--- packages/waystone-components/src/card/card.test.tsx
+++ packages/waystone-components/src/card/card.test.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import path from 'path';
 import { render } from '../common';
 import { Card } from './card';
+import { CoverImageCssOptions } from './card.styles';
 
 describe('The Card component', () => {
   it('Matches snapshot', () => {
     const { container } = render(<Card>card</Card>);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Renders the cover image', () => {
+    const coverImage: CoverImageCssOptions = {
+      url: path.basename('./assets/example-image.png'),
+    };
+    const { container } = render(<Card coverImage={coverImage}>card</Card>);
 
     expect(container).toMatchSnapshot();
   });
