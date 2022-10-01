9002949b3f117996acf6f78318250461d63fba89
William Pei <draekien@users.noreply.github.com>
2022-10-02 09:38:25 +1100

    test: do not test dist folder

--- packages/waystone-components/jest.config.js
+++ packages/waystone-components/jest.config.js
@@ -2,4 +2,20 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jsdom',
+  verbose: true,
+  resetMocks: true,
+  moduleDirectories: ['node_modules'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom',
+    '@testing-library/jest-dom/extend-expect',
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/**/*.{style,styles}.ts',
+    '!<rootDir>/src/**/*.{stories,test,spec}.{ts,tsx}',
+    '!<rootDir>/src/**/*.{constants,models}.ts',
+    '!<rootDir>/src/**/*.generated.{ts,js}',
+    '!<rootDir>/src/*.{ts,tsx}',
+    '!<rootDir>/**/{models,router,constants,apis}/**',
+  ],
 };
