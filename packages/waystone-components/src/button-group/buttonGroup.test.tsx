e293e7dfa50eaffb65e12a40153cf17e3e2be557
William Pei <draekien@users.noreply.github.com>
2022-10-09 19:01:28 +1100

    test(button-group): add tests

---
+++ packages/waystone-components/src/button-group/buttonGroup.test.tsx
@@ -0,0 +1,50 @@
+import { screen, render, fireEvent } from '../__fixtures__';
+import { ButtonGroup } from './buttonGroup';
+
+describe('The ButtonGroup component', () => {
+  it('should match snapshot', () => {
+    const { container } = render(
+      <ButtonGroup
+        buttons={[
+          { id: 'button1', children: 'button1' },
+          { id: 'button2', children: 'button2' },
+        ]}
+      />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should throw an error when no ids are provided for buttons', () => {
+    expect(() =>
+      render(
+        <ButtonGroup
+          buttons={[
+            {
+              children: 'button',
+            },
+          ]}
+        />
+      )
+    ).toThrowError('You must provide an `id` for each button');
+  });
+
+  it('invokes `onChange` when a button is clicked', () => {
+    const handleChange = jest.fn();
+    render(
+      <ButtonGroup
+        buttons={[
+          { id: 'button1', children: 'button1' },
+          { id: 'button2', children: 'button2' },
+        ]}
+        onChange={handleChange}
+      />
+    );
+
+    const button1 = screen.getByText('button1');
+
+    fireEvent.click(button1);
+
+    expect(handleChange).toBeCalled();
+  });
+});
