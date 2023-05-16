4dcc9927ba9ee6dd031eb5b9b746b8a5d034bf67
William Pei <draekien@users.noreply.github.com>
2023-05-17 09:01:37 +1000

    test(cardbutton): add tests

---
+++ packages/waystone-components/src/card-button/cardButton.test.tsx
@@ -0,0 +1,22 @@
+import { fireEvent, render, screen } from '../__fixtures__';
+import { CardButton } from './cardButton';
+
+describe('TThe CardButton component', () => {
+  it('Matches snapshot', () => {
+    const { container } = render(<CardButton>card</CardButton>);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Invokes the onClick callback when clicked', async () => {
+    const handleClick = jest.fn();
+
+    render(<CardButton onClick={handleClick}>card</CardButton>);
+
+    const button = await screen.findByText('card');
+
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
