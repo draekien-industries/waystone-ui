4fcdf64ff80d46157919ae6ddcb0831e458f9240
William Pei <draekien@users.noreply.github.com>
2023-07-26 16:12:49 +1000

    ci(test): set test script to pass with no tests

--- apps/stories/.storybook/main.ts
+++ apps/stories/.storybook/main.ts
@@ -1,18 +1,17 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import path from 'path';
+import path, { dirname, join } from 'path';
 import { mergeConfig } from 'vite';
-
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-    'storybook-dark-mode',
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('storybook-dark-mode'),
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
   core: {
@@ -50,5 +49,11 @@ const config: StorybookConfig = {
     });
   },
 };
-
 export default config;
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
