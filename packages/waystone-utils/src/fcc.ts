24d748123b0d2da8b9965cc59b5be1c93fe038f7
William Pei <draekien@users.noreply.github.com>
2024-03-20 17:58:20 +1100

    chore: update peer deps constraints

--- packages/waystone-utils/src/fcc.ts
+++
@@ -1,8 +0,0 @@
-import type { FC, PropsWithChildren } from 'react';
-import type { EmptyObject } from 'type-fest';
-
-/**
- * Shorthand typing for a React Functional Component
- * that supports children as a prop.
- */
-export type FCC<P = EmptyObject> = FC<PropsWithChildren<P>>;
