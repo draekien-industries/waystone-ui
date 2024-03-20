c9d3e65f9cf219f328af765c5b1837e50e19e3b6
William Pei <draekien@users.noreply.github.com>
2024-03-20 15:19:15 +1100

    build: mark internal packages as private

--- packages/waystone-core/src/utils/makeTheme.ts
+++ packages/waystone-core/src/utils/makeTheme.ts
@@ -1,3 +1,6 @@
 import type { WaystoneTheme } from '../theme';
 
-export const makeTheme = <T extends WaystoneTheme>(theme: T): T => theme;
+export type MakeThemeFn = <T extends WaystoneTheme>(theme: T) => T;
+
+export const makeTheme: MakeThemeFn = <T extends WaystoneTheme>(theme: T) =>
+  theme;
