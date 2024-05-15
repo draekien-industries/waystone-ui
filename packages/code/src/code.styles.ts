bc370d22c2920284275ea215fb1d7cf0739d706c
William Pei <draekien@users.noreply.github.com>
2024-05-15 23:09:22 +1000

    test: add code tests

---
+++ packages/code/src/code.styles.ts
@@ -0,0 +1,12 @@
+import type { ThemeUIStyleObject } from 'theme-ui';
+
+export const codeCss = {
+  variant: 'text.mono',
+  fontSize: '1em',
+  fontWeight: 'medium',
+  backgroundColor: 'ghost',
+  borderRadius: 'small',
+  paddingX: 'extra-small',
+  paddingY: 2,
+  color: 'code',
+} as const satisfies ThemeUIStyleObject;
--- packages/code/src/code.tsx
+++ packages/code/src/code.tsx
@@ -1,17 +1,6 @@
 import type { PropsWithChildren } from 'react';
+import { codeCss } from './code.styles';
 
 export const Code = ({ children }: PropsWithChildren) => (
-  <code
-    sx={{
-      variant: 'text.mono',
-      fontSize: '1em',
-      fontWeight: 'medium',
-      backgroundColor: 'ghost',
-      borderRadius: 'small',
-      paddingX: 'extra-small',
-      paddingY: 2,
-      color: 'code',
-    }}>
-    {children}
-  </code>
+  <code sx={codeCss}>{children}</code>
 );
