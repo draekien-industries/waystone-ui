6703fd5bbd246b35e2d0c60f53fdcf59570223c2
William Pei <draekien@users.noreply.github.com>
2024-05-15 17:53:39 +1000

    feat: update ui package to re-export components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,24 @@
-export * from '@waystone/syntax-highlighter';
-
+// theme
 export * from '@waystone/theme';
+export * from '@waystone/theme-provider';
+
+// CSS
+export * from '@waystone/color';
+export * from '@waystone/css-presets';
+
+// types
+export * from '@waystone/types';
 
 // components
 export * from '@waystone/anchor';
+export * from '@waystone/button';
+export * from '@waystone/button-group';
+export * from '@waystone/card';
+export * from '@waystone/card-button';
+export * from '@waystone/code';
+export * from '@waystone/code-block';
+export * from '@waystone/form-fields';
+export * from '@waystone/icon';
+export * from '@waystone/overlay';
+export * from '@waystone/table';
+export * from '@waystone/text';
